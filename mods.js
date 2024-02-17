7b8f600ec6a3c12da77d6232a6ed31198a1dcd26
Ethan <ethpsa09@gmail.com>
2024-02-17 19:55:39 +0800

    fix game files

--- mods.js
+++ mods.js
@@ -1,4 +1,5 @@
 zipMods = [
+  ['valve.zip', 'Half-Life', 282988036, 27, 10485760],
   ['hl.zip', 'Half-Life', 214283501, 21, 10485760],
   ['opposing_force_en.zip', 'Dont Select, broken', 221191140, 22, 10485760],
   ['halva_rus.zip', 'Dont Select, broken', 303881453, 29, 10485760, 10485760],
@@ -10,9 +11,13 @@ pkgMods = [
 var selectZip = document.getElementById('selectZip');
 var iArgs = document.getElementById('iArgs');
 selectZip.addEventListener('change', function() {
-  if (selectZip.value == "hl.zip") {
+  if (selectZip.value == "valve.zip") {
     //alert('Bugs: menu lagging.');
-    iArgs.value = "-dev 1 -game valve -width 1280 -height 720 +_sethdmodels 0";
+    iArgs.value = "-dev 1 -game valve -width 1280 -height 720 -freq 999 +_sethdmodels 0 +fps_max 999999";
+  }
+  else if (selectZip.value == "hl.zip") {
+    //alert('Bugs: menu lagging.');
+    iArgs.value = "-dev 1 -game valve -width 1280 -height 720 -freq 999 +_sethdmodels 0 +fps_max 999999";
   }
   else if (selectZip.value == "blue_shift_en.zip") {
     //alert('Bugs: Sometimes needs noclip, because you can\'t pick armor');
